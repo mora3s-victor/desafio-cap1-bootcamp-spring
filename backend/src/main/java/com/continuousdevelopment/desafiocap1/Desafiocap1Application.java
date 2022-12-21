315030fd0a69247ae96827af06cb02c834ca62b5
victor moraes <victorhardflipmoraes@outlook.com>
2022-12-21 02:04:12 -0400

    Project created

---
+++ backend/src/main/java/com/continuousdevelopment/desafiocap1/Desafiocap1Application.java
@@ -0,0 +1,13 @@
+package com.continuousdevelopment.desafiocap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiocap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafiocap1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/continuousdevelopment/desafiocap1/Desafiocap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuousdevelopment.desafiocap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafiocap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
